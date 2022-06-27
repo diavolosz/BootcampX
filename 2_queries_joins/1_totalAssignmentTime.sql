f2ead21fdc7c3c780d6252ec7845230ea8d857be
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-27 20:19:42 +0000

    added join then query excercise

---
+++ 2_queries_joins/1_totalAssignmentTime.sql
@@ -0,0 +1,12 @@
+
+
+SELECT SUM(assignment_submissions.duration) as totalDuration
+  FROM students 
+  JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+  WHERE students.name = 'Ibrahim Schimmel';
+
+
+-- SELECT sum(assignment_submissions.duration) as total_duration
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.name = 'Ibrahim Schimmel';
